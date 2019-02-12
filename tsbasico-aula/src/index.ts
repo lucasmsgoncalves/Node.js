45e890786a970847778ae98a8f934b0437255209
Lucas <goncalves.lucasmatheus@gmail.com>
2019-02-12 21:27:03 -0200

    Exercicio01

---
+++ tsbasico-aula/src/index.ts
@@ -0,0 +1,5 @@
+import { OlaMundo } from './olaMundo';
+import { Perguntas } from './perguntas';
+
+// new OlaMundo().run();
+new Perguntas().asks();
---
+++ tsbasico-aula/src/olaMundo.ts
@@ -0,0 +1,13 @@
+export class OlaMundo {
+
+    variavelClasse : boolean  = false;
+    public run(){
+        let numero : number = 7;
+
+        if(numero === 7){
+            let segundaVariavel : boolean = false;
+            numero = 10;
+        }
+        console.log("Olá Mundo!");
+    }
+}
